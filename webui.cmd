634571205c548699894b84904d18147a511be3c8
Himawari G <github@gsolo.com>
2022-08-27 16:47:30 -0400

    Implement hlky UI

--- webui.cmd
+++ webui.cmd
@@ -1,6 +1,14 @@
 @echo off
+call C:\ProgramData\miniconda3\Scripts\activate.bat
+call conda env create -f environment.yaml
+call conda env update --file environment.yaml --prune
 call C:\ProgramData\miniconda3\Scripts\activate.bat ldm
-python "%CD%"\scripts\webui.py
+python "%CD%"\scripts\relauncher.py
 
 :PROMPT
-python scripts/webui.py
+set SETUPTOOLS_USE_DISTUTILS=stdlib
+IF EXIST "models\ldm\stable-diffusion-v1\model.ckpt" (
+  python scripts/relauncher.py
+) ELSE (
+  ECHO Your model file does not exist! Place it in 'models\ldm\stable-diffusion-v1' with the name 'model.ckpt'.
+)
