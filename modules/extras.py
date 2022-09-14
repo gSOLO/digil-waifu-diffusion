781d47038f302a3ec20274dc79cce7051717b95b
Himawari G <github@gsolo.com>
2022-09-14 05:41:43 -0400

    Update Modules

--- modules/extras.py
+++ modules/extras.py
@@ -6,6 +6,8 @@ from modules.shared import opts
 import modules.gfpgan_model
 from modules.ui import plaintext_to_html
 import modules.codeformer_model
+import piexif
+
 
 cached_images = {}
 
@@ -67,14 +69,26 @@ def run_extras(image, gfpgan_visibility, codeformer_visibility, codeformer_weigh
     while len(cached_images) > 2:
         del cached_images[next(iter(cached_images.keys()))]
 
-    images.save_image(image, outpath, "", None, info=info, extension=opts.samples_format, short_filename=True, no_prompt=True, pnginfo_section_name="extras", existing_info=existing_pnginfo)
+    images.save_image(image, path=outpath, basename="", seed=None, prompt=None, extension=opts.samples_format, info=info, short_filename=True, no_prompt=True, grid=False, pnginfo_section_name="extras", existing_info=existing_pnginfo)
 
     return image, plaintext_to_html(info), ''
 
 
 def run_pnginfo(image):
+    items = image.info
+
+    if "exif" in image.info:
+        exif = piexif.load(image.info["exif"])
+        exif_comment = (exif or {}).get("Exif", {}).get(piexif.ExifIFD.UserComment, b'')
+        exif_comment = exif_comment.decode("utf8", 'ignore')
+        items['exif comment'] = exif_comment
+
+        for field in ['jfif', 'jfif_version', 'jfif_unit', 'jfif_density', 'dpi', 'exif']:
+            del items[field]
+
+
     info = ''
-    for key, text in image.info.items():
+    for key, text in items.items():
         info += f"""
 <div>
 <p><b>{plaintext_to_html(str(key))}</b></p>
